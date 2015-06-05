f80b9a6041b227ecbfae6668704d5be43e1e9322
Max Wilson <wilson.max@gmail.com>
2015-06-04 23:20:33 -0700

    Consume hello world

---
+++ Roll/DiceParser.fs
@@ -0,0 +1,20 @@
+module DiceParser
+
+open Rolls
+open System.Text.RegularExpressions
+open Microsoft.FSharp.Text.Lexing
+
+type Command = QuitCommand | RollCommand of RollSpec
+
+let ParseDice input =
+    let lexbuf = LexBuffer<char>.FromString input
+    let parsed = Parser.start Lexer.tokenstream lexbuf
+    printfn "%A" parsed
+    Rolls.Roll(1, 6, 1)
+
+let Parse line =
+    match (line : string).Trim().ToLowerInvariant() with
+    | "quit" | "q" -> QuitCommand
+    | input -> RollCommand(ParseDice input)
+
+    
